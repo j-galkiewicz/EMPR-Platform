6aa54ae69da18cc665c845cbb16fa2259285896f
J. Galkiewicz <46644880+j-galkiewicz@users.noreply.github.com>
2021-02-21 12:57:29 +0100

    login page

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -15,16 +15,23 @@ export default function LoginForm() {
         <form className="flex--Formwraper">
           <label className="input--email">
             E-mail
-            <input type="e-mail"></input>
+            <input type="e-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}>
+            </input>
           </label>
           <label className="input--password">
             Password
-            <input type="password"></input>
+            <input type="password" 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)}>
+            </input>
           </label>
           <Button
             className="btnSubmit"
             type="submit"
             name="Come in"
+            disabled={!validateForm()}
           />
         </form>
       </div>
