65c7e375153aa84967d6e2f8678f5718fb13dc80
J. Galkiewicz <46644880+j-galkiewicz@users.noreply.github.com>
2021-02-24 20:44:58 +0100

    footer v2

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,9 +4,9 @@ import "./_footer.scss";
 const Footer = () => (
     <footer className="footer">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-           <a target="_blank" rel="noreferrer" className="fa fa-facebook" href="https://www.facebook.com" />
-           <a target="_blank"  rel="noreferrer" className="fa fa-instagram" href="https://www.instagram.com" />
-           <a target="_blank" rel="noreferrer" className="fa fa-pinterest" href="https://pl.pinterest.com" />
+           <a target="_blank" rel="noreferrer" className="fa fa-linkedin" href="https://linkedin.com" />
+           <a target="_blank"  rel="noreferrer" className="fa fa-instagram" href="https://instagram.com" />
+           <a target="_blank" rel="noreferrer" className="fa fa-twitter" href="https://twitter.com" />
     </footer>
 );
 export default Footer;
