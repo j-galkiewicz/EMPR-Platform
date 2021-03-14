9191bfec4b77a6049929f91e7d7f9341efc2826c
J. Galkiewicz <46644880+j-galkiewicz@users.noreply.github.com>
2021-03-14 12:48:05 +0100

    responsive menu login button

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -5,6 +5,7 @@ const Menu = () => {
     return (
         <nav className="head__nav">
             <ul>
+                <li><a className="head__nav--item-login" href="/login">Log in</a></li>   
                 <li><a className="head__nav--item" href="/home">Home</a></li>
                 <li><a className="head__nav--item" href="/about">About</a></li>
                 <li><a className="head__nav--item" href="/yourAccount">Your account</a></li>
