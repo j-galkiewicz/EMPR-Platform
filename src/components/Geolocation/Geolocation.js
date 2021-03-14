d6feec653743b4072bcffd9f052091bdef3d5908
j-glkiewicz <jusgalkiewicz@gmail.com>
2021-03-14 20:02:28 +0100

    geo

--- src/components/Geolocation/Geolocation.js
+++ src/components/Geolocation/Geolocation.js
@@ -57,6 +57,10 @@ const Geolocation = () => {
       
         <div className="geo-box1">
           <p className="geoDetails">Organization details:</p>
+            <span className="geoDetails--more"> 
+              <p>Name</p>
+            </span>
+            <br></br>
           <label className="geoDetails">Choose date (calendar)</label>
           <input type="date" id="appointment" name="appointment" />
           <Button className="btnGeo" name="Make an appointment" link="/"></Button> 
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -4,6 +4,9 @@ import React from 'react';
 const Menu = () => {
     return (
         <nav className="head__nav">
+        <div class="toggle">
+            <i class="fa fa-bars menu" aria-hidden="true"></i>
+        </div>
             <ul>
                 <li><a className="head__nav--item-login" href="/login">Log in</a></li>   
                 <li><a className="head__nav--item" href="/home">Home</a></li>
