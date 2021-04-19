e9080d4f638764aca295c2f4556048293678cb93
j-glkiewicz <jusgalkiewicz@gmail.com>
2021-04-19 18:05:57 +0200

    Data Base update

--- src/components/Geolocation/locationsDB.js
+++ src/components/Geolocation/locationsDB.js
@@ -1,20 +1,87 @@
 export default [
     {
-        name: "Frankfurt hosptial",
-        lat: 50.110924,
-        lng: 8.682127,
-        description: "The best place"
+        name: "German Red Cross Blood Donation Center Blutspendedienst- Frankfurt",
+        lat: 50.09241698555217, 
+        lng: 8.653581015343596,
+        description: "DRK-Blutspendedienst- Blutspendezentrum "
     },
     {
-        name: "Berlin hosptial",
-        lat: 52.110924,
-        lng: 12.682127,
-        description: "Blood premium"
+        name: "DRK-Frankfurt Dienstleistungsgesellschaft mbH",
+        lat: 50.125145752271465, 
+        lng: 8.691980129600761,
+        description: "Roten Kreuz"
     },
     {
-        name: "XXXX hosptial",
-        lat: 47.110924,
-        lng: 13.682127,
-        description: "Blood premium"
+        name: "Blut- und Plasmaspende Gießen TMD GmbH",
+        lat: 50.59315940121996, 
+        lng: 8.665086411410426,
+        description: "Kassel"
+    },
+    {
+        name: "Blutspendedienst Rotes Kreuz",
+        lat: 51.33862666253842, 
+        lng: 9.50961695240624,
+        description: "Kassel Rotes Kreuz"
+    },
+    {
+        name: "DRK-Blutspendedienst Baden-Württemberg",
+        lat: 51.339913520532434, 
+        lng: 9.511676888898851,
+        description: "Hessen gGmbH"
+    },
+    {
+        name: "DRK Bereitschaft Neckargemünd",
+        lat: 49.38323194218827, 
+        lng: 8.793106516386533,
+        description: "Wiesenbach"
+    },
+    {
+        name: "DRK-Blutspendedienst Rheinland",
+        lat: 49.83461159827846, 
+        lng: 7.847613252406242,
+        description: "Pfalz und Saarland gGmbH"
+    },
+    {
+        name: "Armed Services Blood Bank Center - Europe",
+        lat: 49.41436246251725, 
+        lng: 7.568256516386536,
+        description: "Armed Services Blood Program ASBP"
+    },
+    {
+        name: "TMD Blood Donation Center Paderborn GmbH",
+        lat: 51.725511880872304, 
+        lng: 8.754547670416093,
+        description: "Blut- und Plasmaspende"
+    },
+    {
+        name: "Blut für's Klinikum",
+        lat: 51.55055298048213, 
+        lng: 9.933838097903777,
+        description: "Universitätsmedizin Göttingen"
+    },
+    {
+        name: "Transfusionszentrale Universitätsmedizin",
+        lat: 50.003877556666744, 
+        lng: 8.260152061411164,
+        description: "Wiesbaden"
+    },
+    {
+        name: "Blutspende",
+        lat: 51.01800875382621, 
+        lng: 11.017695579893925,
+        description: "Erfurt"
+    },
+    {
+        name: "Blood Center of Katharinenhospital",
+        lat: 48.889557613974524, 
+        lng: 9.205786936894683,
+        description: "Klinikum Stuttgart"
+    },
+    {
+        name: "Blutspendedienst des Bayerischen Roten Kreuzes",
+        lat: 48.144913325443255, 
+        lng: 11.546224932417168,
+        description: "München"
     },
 ]
+
